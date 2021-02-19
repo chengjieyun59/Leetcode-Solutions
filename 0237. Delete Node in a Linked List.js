6e15addcc56e76886d05bed532c327056602b263
chengjieyun59 <chengjieyun59@gmail.com>
2021-02-19 14:34:28 -0800

    linked list copy value

---
+++ 0237. Delete Node in a Linked List.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * copy next node's value to current node, skip the next node
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+  node.val = node.next.val
+  node.next = node.next.next
+};
